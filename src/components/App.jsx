40804b59dbae7a12d09308027b77a17ad458aa76
guilhermegalvaosilva <gui.galvaosimplicio@gmail.com>
2025-05-03 20:35:44 +0100

    Cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,37 +18,31 @@ function App() {
   const [cards, setCards] = useState([
     {
       _id: "1",
-      isLiked: false,
-      name: "Yosemite Valley",
+      name: "Vale de Yosemite",
       link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
     },
     {
       _id: "2",
-      isLiked: false,
-      name: "Lake Louise",
+      name: "Lago Louise",
       link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
     },
     {
       _id: "3",
-      isLiked: false,
       name: "Montanhas Carecas",
       link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
     },
     {
       _id: "4",
-      isLiked: false,
       name: "Latemar",
       link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
     },
     {
       _id: "5",
-      isLiked: false,
       name: "Parque Nacional da Vanoise",
       link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
     },
     {
       _id: "6",
-      isLiked: false,
       name: "Lago di Braies",
       link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
     },
