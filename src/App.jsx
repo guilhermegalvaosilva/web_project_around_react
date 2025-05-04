1a090c4062cf72ca2ab6ac44b80a9f8ee18bb48f
guilhermegalvaosilva <gui.galvaosimplicio@gmail.com>
2025-05-04 14:11:26 +0100

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,104 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Header from "./components/Header/Header";
-import Main from "./components/Main";
+import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
-import EditProfilePopup from "./components/EditProfilePopup";
-import AddPlacePopup from "./components/AddPlacePopup";
-import EditAvatarPopup from "./components/EditAvatarPopup";
-import ImagePopup from "./components/ImagePopup";
-import PopupConfirmation from "./components/PopupConfirmation";
+import EditAvatar from "./components/EditAvatar/EditAvatar";
+import EditProfile from "./components/EditProfile/EditProfile";
 
 function App() {
-  const [isEditProfileOpen, setEditProfileOpen] = useState(false);
-  const [isAddPlaceOpen, setAddPlaceOpen] = useState(false);
-  const [isEditAvatarOpen, setEditAvatarOpen] = useState(false);
-  const [selectedCard, setSelectedCard] = useState(null);
+  const [user, setUser] = useState({
+    name: "Guilherme",
+    about: "Web Developer",
+  });
+  const [isAddCardOpen, setIsAddCardOpen] = useState(false);
+  const [isEditProfileOpen, setIsEditProfileOpen] = useState(false);
+  const [isEditAvatarOpen, setIsEditAvatarOpen] = useState(false);
 
-  function handleEditProfileClick() {
-    setEditProfileOpen(true);
+  const [cards, setCards] = useState([
+    {
+      _id: "1",
+      name: "Vale de Yosemite",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+    },
+    {
+      _id: "2",
+      name: "Lago Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+    },
+    {
+      _id: "3",
+      name: "Montanhas Carecas",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+    },
+    {
+      _id: "4",
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+    },
+    {
+      _id: "5",
+      name: "Parque Nacional da Vanoise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+    },
+    {
+      _id: "6",
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+    },
+  ]);
+
+  function handleOpenAddCard() {
+    setIsAddCardOpen(true);
   }
 
-  function handleAddPlaceClick() {
-    setAddPlaceOpen(true);
+  function handleCloseAddCard() {
+    setIsAddCardOpen(false);
   }
 
-  function handleEditAvatarClick() {
-    setEditAvatarOpen(true);
+  function handleDeleteCard(card) {
+    const filteredCards = cards.filter((c) => c._id !== card._id);
+    setCards(filteredCards);
   }
 
-  function handleCardClick(card) {
-    setSelectedCard(card);
+  function handleEditUserPopup() {
+    setIsEditProfileOpen((prev) => !prev);
   }
 
-  function closeAllPopups() {
-    setEditProfileOpen(false);
-    setAddPlaceOpen(false);
-    setEditAvatarOpen(false);
-    setSelectedCard(null);
+  function handleUpdateUserInfo(updatedUser) {
+    setUser(updatedUser);
+  }
+
+  function handleOpenEditAvatar() {
+    console.log("Abriu o popup de avatar");
+    setIsEditAvatarOpen(true);
+  }
+
+  function handleCloseEditAvatar() {
+    setIsEditAvatarOpen(false);
   }
 
   return (
-    <div className="page">
-      <div className="page__content">
-        <Header />
-        <Main
-          onEditProfile={handleEditProfileClick}
-          onAddPlace={handleAddPlaceClick}
-          onEditAvatar={handleEditAvatarClick}
-          onCardClick={handleCardClick}
-        />
-        <Footer />
-        <EditProfilePopup isOpen={isEditProfileOpen} onClose={closeAllPopups} />
-        <AddPlacePopup isOpen={isAddPlaceOpen} onClose={closeAllPopups} />
-        <EditAvatarPopup isOpen={isEditAvatarOpen} onClose={closeAllPopups} />
-        <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-        <PopupConfirmation isOpen={false} onClose={closeAllPopups} />
-      </div>
+    <div className="page__content">
+      <Header />
+
+      <Main
+        cards={cards}
+        user={user}
+        handleEditUserPopup={handleEditUserPopup}
+        handleDeleteCard={handleDeleteCard}
+        handleOpenAddCard={handleOpenAddCard}
+        handleCloseAddCard={handleCloseAddCard}
+        isAddCardOpen={isAddCardOpen}
+        handleOpenEditAvatar={handleOpenEditAvatar}
+      />
+
+      <EditProfile
+        isOpen={isEditProfileOpen}
+        onClose={handleEditUserPopup}
+        onUpdateUser={handleUpdateUserInfo}
+      />
+      <EditAvatar isOpen={isEditAvatarOpen} onClose={handleCloseEditAvatar} />
+      <Footer />
     </div>
   );
 }
